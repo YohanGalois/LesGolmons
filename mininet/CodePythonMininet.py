98881f6ac2038cc79dc08bd28fd8d431987e7cb5
AntoineAubert <aubert.antoi@gmail.com>
2024-06-18 17:54:11 +0200

    toto2

--- mininet/CodePythonMininet.py
+++ mininet/CodePythonMininet.py
@@ -1,28 +1,23 @@
-#!/usr/bin/python
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import RemoteController
+from mininet.util import dumpNodeConnections
+from mininet.cli import CLI
 import time
 
 
 class Mytopo(Topo):
-	
 	def __init__(self):
 		"Create custom topo."
-		
 		# Initialize topo
 		Topo.__init__(self)
-		
 		###### Create the first sub-network (servers part)
 		# Network nodes
 		web_server	= self.addHost("web")
 		traff1		= self.addHost("traff1")
 		traff2		= self.addHost("traff2")
-		
 		# The Switch 
 		s1			= self.addSwitch("s1")
-		
 		###### Create the scond sub-network (hots part)
 		# Network nodes
 		# ~ attacker	= self.addHost("attacker")
@@ -30,7 +25,7 @@ class Mytopo(Topo):
 		# ~ client2		= self.addHost("client2")
 		
 		# The Switch 
-		s2			= self.addSwitch("s2")
+		# ~ s2			= self.addSwitch("s2")
 		
 		# The aggregator switch
 		# ~ s3		= self.addSwitch("s3")
@@ -44,15 +39,22 @@ class Mytopo(Topo):
 		# ~ self.addLink(client2, s2)
 		# ~ self.addLink(s1,s3)
 		# ~ self.addLink(s2,s3)
-		
-					
 def run():
-	c0 = RemoteController('c0', '192.168.122.3', 6633)
-	my_topo = Mytopo()
-	net = Mininet(topo=my_topo, controller=None)
-	net.addController(c0)
-	net.start()
-	net.stop()
-	
+        c0 = RemoteController('c0', '192.168.12.25', 6634)
+        my_topo = Mytopo()
+        net = Mininet(topo=my_topo, controller=None)
+        net.addController(c0)
+        net.start()
+        web = net.get("web")
+        web.cmd('python -m SimpleHTTPServer 8000 &')
+        client = net.get("traff1")
+        print("try a wget ...")
+        cmd_exe = 'wget -o - '+str(web.IP())+':8000'
+        # print(cmd_exe)
+        for i in range(500):
+            res = client.cmd(cmd_exe)
+            print(res)
+        # CLI(net)
+        net.stop()
 if __name__ == '__main__':
 	run()
